04cf39ed4ba3baddb4a9d6d0f95b0bce8093d525
24wings <731502029@qq.com>
2019-04-27 12:04:32 +0800

    data

--- server/Projects/Config/WechatConfig.cs
+++ server/Projects/Config/WechatConfig.cs
@@ -1,22 +1,29 @@
-namespace Wings.Projects.Config {
+namespace Wings.Projects.Config
+{
     /// <summary>
     /// 微信配置
     /// </summary>
-    public class WechatConfig {
+    public class WechatConfig
+    {
+        /// <summary>
+        /// AppId
+        /// </summary>
+        /// <value></value>
+        public static string AppId { get; } = "wx78e5285e4bef6c54";
         /// <summary>
         /// url
         /// </summary>
         /// <value></value>
-        public string Url { get; } = "http://115.29.64.6";
+        public static string Url { get; } = "http://115.29.64.6";
         /// <summary>
         /// 微信Token
         /// </summary>
         /// <value></value>
-        public string Token { get; } = "uWVmVXPM1mY71lYwPsSs38Zn1ys1zqux";
+        public static string Token { get; } = "uWVmVXPM1mY71lYwPsSs38Zn1ys1zqux";
         /// <summary>
         /// 消息加密密钥由43位字符组成，可随机修改，字符范围为A-Z，a-z，0-9。
         /// </summary>
         /// <value></value>
-        public string EncodingAESKey { get; } = "g4BZtbw4H2Z5A8A1B94d9T5bkA5WAWn1DZ1ANb1289B";
+        public static string EncodingAESKey { get; } = "g4BZtbw4H2Z5A8A1B94d9T5bkA5WAWn1DZ1ANb1289B";
     }
 }
--- server/Projects/Wechat/WechatController.cs
+++ server/Projects/Wechat/WechatController.cs
@@ -1,22 +1,59 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
+using Wings.Projects.Config;
+using Senparc.CO2NET.Extensions;
+using Senparc.Weixin.Exceptions;
+using Senparc.Weixin.HttpUtility;
+using Senparc.Weixin.MP.AdvancedAPIs;
+using Senparc.Weixin.MP.AdvancedAPIs.OAuth;
+using Senparc.Weixin.MP.CommonAPIs;
+using Senparc.Weixin.MP;
 
-namespace Wings.Projects.Wechat {
+namespace Wings.Projects.Wechat
+{
     /// <summary>
     /// 组织管理
     /// </summary>
-    [Route ("/api/wechat")]
-    public class WechatController {
+    [Route("/api/wechat")]
+    public class WechatController : Controller
+    {
 
         /// <summary>
-        /// 微信授权认证
+        /// 微信接入
         /// </summary>
         /// <returns></returns>
-        [HttpGet ("[action]")]
-        public object auth (string signature, int timestamp, string nonce, string echostr) {
-            Console.WriteLine ("登录");
+        [HttpGet("[action]")]
+        public object auth(string signature, int timestamp, string nonce, string echostr)
+        {
+            Console.WriteLine("登录");
 
             return echostr;
+        }
+
+        /// <summary>
+        /// 微信授权
+        /// </summary>
+        /// <param name="returnUrl"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public object oauth(string returnUrl)
+        {
+            var state = "JeffreySu-" + DateTime.Now.Subtract(new DateTime(1970, 1, 1, 1, 0, 0, 0)).TotalMilliseconds;//随机数，用于识别请求可靠性
+            // Session["State"] = state;//储存随机数到Session
+
+            ViewData["returnUrl"] = returnUrl;
+
+            //此页面引导用户点击授权
+            ViewData["UrlUserInfo"] =
+                OAuthApi
+                .GetAuthorizeUrl(WechatConfig.AppId,
+                "https://sdk.weixin.senparc.com/oauth2/UserInfoCallback?returnUrl=" + returnUrl.UrlEncode(),
+                state, OAuthScope.snsapi_userinfo);
+            ViewData["UrlBase"] =
+                OAuthApi.GetAuthorizeUrl(WechatConfig.AppId,
+                "https://sdk.weixin.senparc.com/oauth2/BaseCallback?returnUrl=" + returnUrl.UrlEncode(),
+                state, OAuthScope.snsapi_base);
+            return View();
         }
     }
 }
