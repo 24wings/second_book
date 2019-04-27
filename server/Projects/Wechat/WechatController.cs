fc94c3338eac5f11d62f2e7bb13bc3900eeda100
24wings <731502029@qq.com>
2019-04-27 13:11:55 +0800

    data

--- server/Projects/Wechat/WechatController.cs
+++ server/Projects/Wechat/WechatController.cs
@@ -35,26 +35,38 @@ namespace Wings.Projects.Wechat
         /// </summary>
         /// <param name="returnUrl"></param>
         /// <returns></returns>
-        [HttpGet]
-        public object oauth(string returnUrl)
+        [HttpGet("oauth2/[action]")]
+        public object getOauthUrl(string returnUrl)
         {
             Console.WriteLine("hahah");
             var state = "JeffreySu-" + DateTime.Now.Subtract(new DateTime(1970, 1, 1, 1, 0, 0, 0)).TotalMilliseconds;//随机数，用于识别请求可靠性
             // Session["State"] = state;//储存随机数到Session
 
-            ViewData["returnUrl"] = returnUrl;
-
             //此页面引导用户点击授权
-            ViewData["UrlUserInfo"] =
+            var UrlUserInfo =
                 OAuthApi
                 .GetAuthorizeUrl(WechatConfig.AppId,
-                "https://sdk.weixin.senparc.com/oauth2/UserInfoCallback?returnUrl=" + returnUrl.UrlEncode(),
+                "http://115.29.64.6/api/wechat/oauth2/UserInfoCallback?returnUrl=" + returnUrl.UrlEncode(),
                 state, OAuthScope.snsapi_userinfo);
-            ViewData["UrlBase"] =
-                OAuthApi.GetAuthorizeUrl(WechatConfig.AppId,
-                "https://sdk.weixin.senparc.com/oauth2/BaseCallback?returnUrl=" + returnUrl.UrlEncode(),
-                state, OAuthScope.snsapi_base);
-            return View();
+            // var UrlBase =
+            //     OAuthApi.GetAuthorizeUrl(WechatConfig.AppId,
+            //     "https://115.29.64.6/api/wechat/oauth2/BaseCallback?returnUrl=" + returnUrl.UrlEncode(),
+            //     state, OAuthScope.snsapi_base);
+
+            return this.Redirect(UrlUserInfo);
         }
+        /// <summary>
+        /// oauth2认证
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("oauth2/[action]")]
+        public object userInfoCallback(string code, string state, string returnUrl)
+        {
+
+            return new { code = code, state = state, returnUrl = returnUrl };
+
+        }
+
+
     }
 }
